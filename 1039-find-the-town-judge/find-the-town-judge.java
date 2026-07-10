4a495b3ec0f8472db6a4a092517dee1aa6ca32e1
swap-408 <60997870+swap-408@users.noreply.github.com>
2026-07-10 09:10:13 +0530

    Time: 3 ms (73.36%) | Memory: 53.7 MB (51.51%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.java
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -1,13 +1,13 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        HashMap<Integer, List<Integer>> graph = new HashMap<>();
+
         int[] freq = new int[n];
         for(int[] e: trust){
-            graph.computeIfAbsent(e[0], x -> new ArrayList<>()).add(e[1]);
+            freq[e[0]-1]--;
             freq[e[1]-1]++;
         }
         for(int i=0;i<n;i++){
-            if(freq[i]==n-1 && !graph.keySet().contains(i+1)) return i+1;
+            if(freq[i]==n-1) return i+1;
         }
         return -1;
     }
