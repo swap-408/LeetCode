8a2930f449a35179ea74653c09d47135b6f3c1a3
swap-408 <60997870+swap-408@users.noreply.github.com>
2026-07-10 07:17:08 +0530

    Time: 9 ms (48.96%) | Memory: 47.9 MB (28.40%) - LeetSync

--- 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.java
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,22 +1,20 @@
 class Solution {
     public int minDeletions(String s) {
         int[] freq = new int[26];
-        int res = 0;
         for(int i=0;i<s.length();i++){
             freq[s.charAt(i)-'a']++;
         }
         Arrays.sort(freq);
-        int p = Integer.MAX_VALUE;
+        int res =0;
+        int prev = Integer.MAX_VALUE;
         for(int i=25;i>=0;i--){
             if(freq[i]==0) break;
-            if(p==0){
-                res += freq[i]; continue;
-            }
-            if(p<=freq[i]){
-                p = p-1;
-                res += freq[i]-p;
+            if(prev == 0) res += freq[i];
+            else if(prev<=freq[i]){
+                res += (freq[i]-prev+1);
+                prev -= 1;
             }else{
-                p=freq[i];
+                prev = freq[i];
             }
         }
         return res;
