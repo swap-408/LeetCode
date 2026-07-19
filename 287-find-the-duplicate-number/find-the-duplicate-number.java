62d7f1f1eca373248a6017f13b8ac8391781cecb
swap-408 <60997870+swap-408@users.noreply.github.com>
2026-07-19 13:11:59 +0530

    Time: 7 ms (40.52%) | Memory: 82.8 MB (58.26%) - LeetSync

---
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            while(nums[i]!=i+1){
+                if(nums[i]==nums[nums[i]-1]) return nums[i];
+                swap(nums,i,nums[i]-1);
+            }
+        }
+        return 0;
+    }
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
