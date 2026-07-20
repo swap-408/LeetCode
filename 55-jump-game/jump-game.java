a071267e79aa700d3a34d3e3e9d201ddb0f83fc5
swap-408 <60997870+swap-408@users.noreply.github.com>
2026-07-20 20:47:03 +0530

    Time: 2 ms (89.01%) | Memory: 48 MB (43.28%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,14 +1,10 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int end =0, far=0;
-        for(int i=0;i<nums.length && i<=end;i++){
-            far = Math.max(far,i+nums[i]);
-            if(i==end){
-                end = far;
-            }
+        int maxReach = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            if (i > maxReach) return false;
+            maxReach = Math.max(maxReach, i + nums[i]);
         }
-
-        if(far>=nums.length-1) return true;
-        return false;
+        return true;
     }
 }
