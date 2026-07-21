8ef8a1b0f422b66376a937235e7b4ebac998a280
swap-408 <60997870+swap-408@users.noreply.github.com>
2026-07-21 15:47:43 +0530

    Time: 1 ms (99.99%) | Memory: 71.2 MB (73.62%) - LeetSync

---
+++ 41-first-missing-positive/first-missing-positive.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n=nums.length, j=0;
+        for(int i=0;i<n;i++){
+            while(nums[i]>=1 && nums[i]<=n && nums[i] != nums[nums[i] - 1]){
+                swap(nums,i,nums[i]-1);
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(i+1!=nums[i]) return i+1;
+        }
+        return n+1;
+    }
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
