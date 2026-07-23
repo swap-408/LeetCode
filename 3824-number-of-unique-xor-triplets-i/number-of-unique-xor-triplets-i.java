5e1563bce9b05f478bd5901511ee8d3660279df2
swap-408 <60997870+swap-408@users.noreply.github.com>
2026-07-23 12:08:13 +0530

    Time: 1 ms (51.02%) | Memory: 125.2 MB (93.88%) - LeetSync

--- 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.java
+++ 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.java
@@ -1,13 +1,8 @@
 class Solution {
     public int uniqueXorTriplets(int[] nums) {
         int n = nums.length;
-        if(n==2) return 2;
-        if(n==1) return 1; 
-        
-        int k=1;
-        while(k<Integer.MAX_VALUE && k<=n){
-            k *= 2;
-        }
-        return k;
+        if(n<3) return n;
+        return Integer.highestOneBit(n)*2;
     }
+        
 }
