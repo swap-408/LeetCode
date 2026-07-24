36c7b6b998f2b0a0bb9647c276f406471ba15f00
swap-408 <60997870+swap-408@users.noreply.github.com>
2026-07-25 04:27:49 +0530

    Time: 32 ms (45.45%) | Memory: 50.9 MB (38.35%) - LeetSync

--- 1967-longest-substring-of-all-vowels-in-order/longest-substring-of-all-vowels-in-order.java
+++ 1967-longest-substring-of-all-vowels-in-order/longest-substring-of-all-vowels-in-order.java
@@ -1,31 +1,19 @@
 class Solution {
-    int start = 0;
     public int longestBeautifulSubstring(String word) {
         int res =0;
-        while(start<word.length()){
-            if(word.charAt(start)=='a'){
-                int temp = seq(word);
-                if(temp != -1) res = Math.max(res,temp);
-            }else start++;
+        int uv = 1;
+        int left =0;
+        for(int right=1;right<word.length();right++){
+            if(word.charAt(right)<word.charAt(right-1)){
+                left = right;
+                uv = 1;
+            }else if(word.charAt(right)>word.charAt(right-1)){
+                uv++;
+            }
+            if(uv==5){
+                res = Math.max(res, right-left+1);
+            }
         }
         return res;
-    }
-    public int seq(String word){
-        int n = word.length();
-        int count = start;
-        while(start < n && word.charAt(start)=='a') start++;
-        if(start==n || word.charAt(start)!='e') return -1;
-
-        while(start < n && word.charAt(start)=='e') start++;
-        if(start==n || word.charAt(start)!='i') return -1;
-        
-        while(start < n && word.charAt(start)=='i') start++;
-        if(start==n || word.charAt(start)!='o') return -1;
-        
-        while(start < n && word.charAt(start)=='o') start++;
-        if(start==n || word.charAt(start)!='u') return -1;
-        
-        while(start < n && word.charAt(start)=='u') start++;
-        return start - count;
     }
 }
