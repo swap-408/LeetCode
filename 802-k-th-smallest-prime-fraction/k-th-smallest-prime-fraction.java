2da796c5d36ca71ec282da8819f38a88caa03138
swap-408 <60997870+swap-408@users.noreply.github.com>
2026-08-03 02:50:54 +0530

    Time: 335 ms (38.49%) | Memory: 85.7 MB (38.62%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.java
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] kthSmallestPrimeFraction(int[] arr, int k) {
         PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> 
-                    Double.compare((1.0)*b[0]/b[1], (1.0)*a[0]/a[1]));
+                    Long.compare((long)b[0]*a[1], (long)a[0]*b[1]));
         for(int i=0;i<arr.length;i++){
             for(int j=i+1;j<arr.length;j++){
                 pq.offer(new int[]{arr[i],arr[j]});
