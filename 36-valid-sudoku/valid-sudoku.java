518eced6b45c5a8a2971e0de4ceeb4f3d4e0ab66
swap-408 <60997870+swap-408@users.noreply.github.com>
2026-08-28 02:07:58 +0530

    Time: 1 ms (99.93%) | Memory: 46.5 MB (54.89%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,26 +1,30 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        List<Set<Integer>> row = new ArrayList<>();
-        List<Set<Integer>> col = new ArrayList<>();
-        List<Set<Integer>> box = new ArrayList<>();
-        for(int i=0;i<9;i++){
-            row.add(new HashSet<>());
-            col.add(new HashSet<>());
-            box.add(new HashSet<>());
-        }
+        int[] rowMasks = new int[9];
+        int[] colMasks = new int[9];
+        int[] boxMasks = new int[9];
+
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
-                if(board[i][j]=='.') continue;
-                int curr = board[i][j]-'0';
-                boolean cond = row.get(i).contains(curr) || col.get(j).contains(curr);
-                cond = cond || box.get(3*(i/3)+j/3).contains(curr);
-                if(cond) return false;
-                row.get(i).add(curr);
-                col.get(j).add(curr);
-                box.get(3*(i/3) + j/3).add(curr);
+                char c = board[i][j];
+                if(c == '.') continue;
+                int value = c - '0';
+                int mask = 1 << (value-1);
+                int boxIndex = 3*(i/3) + j/3;
+                
+                boolean hasEntry = ((rowMasks[i] & mask) != 0) ||
+                                    ((colMasks[j] & mask) != 0) ||
+                                    ((boxMasks[boxIndex] & mask) != 0);
+
+                if(hasEntry) return false;
+                
+                rowMasks[i] |= mask;
+                colMasks[j] |= mask;
+                boxMasks[boxIndex] |= mask;
             }
         }
-        return true;
 
+        return true;
+        
     }
 }
